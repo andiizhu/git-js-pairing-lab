a24d97bca4f8a007332e8503cff1bce420717d91
andiizhu <azhu0317@gmail.com>
2024-03-28 13:55:11 -0400

    Reset

--- index.js
+++
@@ -1,9 +0,0 @@
-//Code your solutions in this file
-//Question 1
-const fiveToOneHundred = () => {
-    for (let i = 5; i <= 100; i++) {
-        console.log(i)
-    }
-    return;
-}
-// fiveToOneHundred()
