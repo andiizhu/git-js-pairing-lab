dc14a765ab1ff11d622315d3004ecb5e26bc1450
andiizhu <azhu0317@gmail.com>
2024-03-28 13:43:23 -0400

    Answered Question 1

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 //Code your solutions in this file
+//Question 1
+const fiveToOneHundred = () => {
+    for (let i = 5; i <= 100; i++) {
+        console.log(i)
+    }
+    return;
+}
+// fiveToOneHundred()
